2f0bf20d4bebbf6533c4ce1646bbba52ac1cd667
danielang1073 <danielcc89@gmail.com>
2023-07-07 13:18:15 -0500

    new picture empresa

---
+++ src/pagesAdmin/HomeAdmin.tsx
@@ -0,0 +1,74 @@
+import { Sidebar } from "../components/Sidebar";
+import logo from "../assets/images/disruptialogo.png";
+
+const HomeAdmin = () => {
+  const infoDivs = [
+    {
+      id: 1,
+      label: "Crear una vacante",
+    },
+    {
+      id: 2,
+      label: "Diagnóstico y formación",
+    },
+    {
+      id: 3,
+      label: "Revisar perfiles de vacantes creadas",
+    },
+  ];
+  return (
+    <>
+      <Sidebar
+        smallTitle="Crear mi Hoja de Vida"
+        title="Hola Andrea"
+        subTitle="Disruptia y Chat GPT, te ayudan a crear tu hoja de vida a un clic"
+        subText=""
+        titleTwo="¡Es tu oportunidad!"
+        backColor={false}
+        img={false}
+      />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+            gap: "30px",
+            padding: "100px 47px 0px 192px",
+          }}
+        >
+          {infoDivs.map((item: any) => (
+            <div
+              style={{
+                border: "solid 1px green",
+                background: "rgb(110,76,143)",
+                borderRadius: "40px",
+                height: "600px",
+                width: "350px",
+                display: "flex",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              <div>
+                <span
+                  style={{
+                    color: "white",
+                    fontFamily: "Montserrat-Bold",
+                    fontSize: "40px",
+                  }}
+                >
+                  {item.label}
+                </span>
+              </div>
+            </div>
+          ))}  
+         </div>         
+      </div>
+      <div  style={{display:"flex",justifyContent: "center",marginTop:"20px",marginLeft:"140px"}}>
+          <img style={{ width: "153px", height: "41px" }} alt="" src={logo} />
+        </div>
+    </>
+  );
+};
+
+export default HomeAdmin;
